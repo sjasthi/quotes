3fd565d9f13e65c9341539b0c3ed666e17202ae0
Jasthi <srj@splm.siemens.com>
2020-12-12 22:30:23 -0600

    Update index.php
    first verson of index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+	
+  $_SESSION['lastpage'] = 'index';
 
   // set the current page to one of the main buttons
   $nav_selected = "HOME";
@@ -9,80 +11,136 @@
   // set the left menu button selected; options will change based on the main selection
   $left_selected = "";
 
-  include("./nav.php");
-
-  $query = "SELECT quote FROM quote_table ORDER BY RAND() LIMIT 1";
-  $query2="SELECT * FROM pref";
+  include("nav.php");
 ?>
 
 <html>
 
 <head>
-<style>
-table.center {
-    margin-left:auto; 
-    margin-right:auto;
-  }
-  span.block {
-	  background-color: black;
-	  position:relative; 
- color: white;	  
-	    margin: 20px;
-  padding: 30px;
-  line-height:1.6;
-  }
-</style>
-<link rel="stylesheet" href="css/cjui.css">
-    <script src="jquery/jquery.js"></script>
-    <script src="jquery/jui.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles/main_style.css" type="text/css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="styles/custom_nav.css" type="text/css">
+  <title>Scrambler Puzzles</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="../css/mainStyleSheet.css">
+  <style>
+    table.puzzle {
+        margin-left:auto; 
+        margin-right:auto;
+        align:center;
+        
+      }
+    table.puzzle tr td.filled {
+      /* width: 48px; */
+      /* height: 48px; */
+      font-size: 12px;
+      text-align: center;
+      border: 3px solid #000000;
+      padding: 5px;
+      font-weight:bold;
+    }
+  </style>
 </head>
 
 <body>
-<h2 style = "color: #01B0F1; margin:40px">Welcome to QuoteMaster </h3>
+
+<div class="container-fluid">
+    <div class="panel">
+        <div class="panel-group">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div align="center"><h2><?php echo 'Welcome to the Quotes! Get inspired!'; ?></h2></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <div align="center">
+                        <h3><?php echo 'What do you want to play?';?></h3>
+                        <br>
+                    </div>
+
+<!-- Hard code the preferences for now; TBD: 
+Eventually, this will be fetched from the preferences table. -->
+<?php
+
+$quotes_to_show = 10;
+
+
+// Get the last "puzzles_to_show" number of word_sets  (or all word_sets if the count is < 100)
+// We just need id, author and topic 
+// SELECT id, author, topic FROM quote_table ORDER BY id DESC LIMIT 20
+$sql = "SELECT id, author, topic FROM quote_table ORDER BY id DESC LIMIT $quotes_to_show";
+
+$result = mysqli_query($db, $sql);
+
+echo '<table class="puzzle">';
+
+
+// in each row, we will display 6 icons
+if($result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    
+    echo '<tr>';
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/drop_quote.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/float_quote.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/drop_float.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/scramble.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/splitter.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+    
+    echo  '<td class="filled"><a href="feelingLucky/feelingLucky.php?id='.$row['id'].'">
+          <img src="./images/slider16.png" height="50" width="50">
+          <br>'.$row['topic'].'<br>'.$row['author'].'</a></td>';
+
+    echo '</tr>';
+  }
+}
+
+echo '</table>';
+
+?>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- Process the mysqli_result now 
+Based on the count of word_sets received (it will be = $puzzles_to_show or all puzzles),
+display the puzzle icons on the home page as follows.
+Show $puzzles_per_row per row. 
+So, by default, users will see the last 100 puzzles with latest one showing up first.
+
+Based on the "type", we can identify the ICON.
+Below the ICON, show the "title" and below the title, show "subtitle"
+Set the HREF link to the ICON as   href="feelinglucky.php?id=N"  where N is the set_id
+So, the users can play any puzzle they want from the home page.
+And random = feeling lucky = feelinglucky.php is same as clicking the first icon on the home page. -->
+
+<!-- </p> -->
 </body>
-<?php 
-header('Content-type: text/html; charset=utf-8');
-$db->set_charset("utf8");
-   $data = mysqli_query($db, $query);
-    $data2 = mysqli_query($db, $query2);
-	
-	 if ($data->num_rows > 0) {
-					
-					 while($row2 = $data2->fetch_assoc()) {
-						 $axe=$row2['display'];
-						 $value =$row2['value'];
-						 
-						if( strcmp($axe,"Both")==0){ $axis=rand(1,2) ;//selects a random variable
-						}else if( strcmp($axe,"Quotes")==0)
-						{$axis=3;
-						}else{$axis=4;}
-					 }
-	
-	 }
-	
-	
-	
-   if ($data->num_rows > 0) {
-					
-					
-					
-					 while($row = $data->fetch_assoc()) {
-						 if($axis==1||$axis==3){
-					echo ' <span class="block">
-					
-					
-					'.$row["quote"].'
-</span> ';					
-					
-						 
-   } else {
-	     include("puzzlemaker.php");
-	   DropM($row["quote"],$value);
-   }
-   }
-   }
- 
- 
- ?>
 
 </html>
