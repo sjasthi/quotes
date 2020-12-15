4477a6b4f2ee98842f7cb4f5875674ea1f4a20a0
Jasthi <srj@splm.siemens.com>
2020-12-15 08:53:58 -0600

    Fixed the preferences update

--- modifyThePreferences.php
+++ modifyThePreferences.php
@@ -2,35 +2,59 @@
 
 include_once 'db_credentials.php';
 
-   $column_count = mysqli_real_escape_string($db, $_POST['column_count']);
-  $language = mysqli_real_escape_string($db, $_POST['language']);
-  $home_page_display = mysqli_real_escape_string($db, $_POST['home_page_display']);
-  $no_of_quotes_to_display = mysqli_real_escape_string($db, $_POST['no_of_quotes_to_display']);
-  $feeling_lucky_mode = mysqli_real_escape_string($db, $_POST['feeling_lucky_mode']);
-  $feeling_lucky_type = mysqli_real_escape_string($db, $_POST['feeling_lucky_type']);
-
-  echo $column_count;
-  echo $language;
+// get the POSTed values fom the preference form
+$column_count = mysqli_real_escape_string($db, $_POST['column_count']);
+$language = mysqli_real_escape_string($db, $_POST['language']);
+$home_page_display = mysqli_real_escape_string($db, $_POST['home_page_display']);
+$no_of_quotes_to_display = mysqli_real_escape_string($db, $_POST['no_of_quotes_to_display']);
+$feeling_lucky_mode = mysqli_real_escape_string($db, $_POST['feeling_lucky_mode']);
+$feeling_lucky_type = mysqli_real_escape_string($db, $_POST['feeling_lucky_type']);
 
 
-  // id name value comments - is the table structure
-  $sql = "UPDATE `preferences` SET `value`= $column_count WHERE `name`= 'DEFAULT_COLUMN_COUNT'";
-  mysqli_query($db, $sql);
-  
-  $sql = "UPDATE `preferences` SET `value`= $language WHERE `name`= 'DEFAULT_LANGUAGE'";
-  mysqli_query($db, $sql);
-  
-  $sql = "UPDATE `preferences` SET `value`= $home_page_display WHERE `name`= 'DEFAULT_HOME_PAGE_DISPLAY'";
-  mysqli_query($db, $sql);
+// id name value comments - is the preferences table structure
+// Update each row with the new value
+$sql = "UPDATE `preferences` SET `value`= $column_count WHERE `name`= 'DEFAULT_COLUMN_COUNT';";
+mysqli_query($db, $sql);
 
-  $sql = "UPDATE `preferences` SET `value`= $no_of_quotes_to_display WHERE `name`= 'NO_OF_QUOTES_TO_DISPLAY'";
-  mysqli_query($db, $sql);
+$sql = "UPDATE `preferences` SET `value`='$language' WHERE `name`= 'DEFAULT_LANGUAGE'";
+echo $sql;
+echo "<br>";
+mysqli_query($db, $sql);
 
-  
-  $sql = "UPDATE `preferences` SET `value`= $feeling_lucky_mode WHERE `name`= 'FEELING_LUCKY_MODE'";
-  mysqli_query($db, $sql);
+$sql = "UPDATE `preferences` SET `value`='$home_page_display' WHERE `name`= 'DEFAULT_HOME_PAGE_DISPLAY'";
+echo $sql;
+echo "<br>";
+mysqli_query($db, $sql);
 
-  $sql = "UPDATE `preferences` SET `value`= $feeling_lucky_type WHERE `name`= 'FEELING_LUCKY_TYPE'";
-  mysqli_query($db, $sql);
+$sql = "UPDATE `preferences` SET `value`= $no_of_quotes_to_display WHERE `name`= 'NO_OF_QUOTES_TO_DISPLAY'";
+echo $sql;
+echo "<br>";
+mysqli_query($db, $sql);
 
-  //header('location: preferences.php?preferencesUpdated=Success');
+
+$sql = "UPDATE `preferences` SET `value`='$feeling_lucky_mode' WHERE `name`= 'FEELING_LUCKY_MODE'";
+echo $sql;
+echo "<br>";
+mysqli_query($db, $sql);
+
+$sql = "UPDATE `preferences` SET `value`= '$feeling_lucky_type' WHERE `name`= 'FEELING_LUCKY_TYPE'";
+echo $sql;
+echo "<br>";
+mysqli_query($db, $sql);
+
+// =========== Refresh $_SESSION varaibles again ==============
+// refresh the $_SESSION variables so that we see the updated values
+$sql = "SELECT `id`, `name`, `value`, `comments` FROM `preferences`";
+mysqli_set_charset($db, "utf8");
+$result = mysqli_query($db, $sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $_SESSION[$row['name']] = $row['value'];
+    } // end while
+} // end if
+// ==============================================
+
+
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('location: preferences.php?preferencesUpdated=Success');
--- preferences.php
+++ preferences.php
@@ -7,106 +7,124 @@ $left_selected = "";
 $page_title = 'Preferences';
 require 'db_credentials.php';
 include("./nav.php");
+
+// Here are the preferences we saved in $_SESSION in index dot php.
+// DEFAULT_COLUMN_COUNT, 
+// DEFAULT_LANGUAGE, 
+// DEFAULT_HOME_PAGE_DISPLAY,  
+// DEFAULT_CHUNK_SIZE, 
+// NO_OF_QUOTES_TO_DISPLAY, 
+// FEELING_LUCKY_MODE, 
+// FEELING_LUCKY_TYPE
+// Retrieve those and show the current values. 
+
+// refresh the $_SESSION variables so that we see the updated values
+$sql = "SELECT `id`, `name`, `value`, `comments` FROM `preferences`";
+mysqli_set_charset($db, "utf8");
+$result = mysqli_query($db, $sql);
+
+if ($result->num_rows > 0) {
+	while ($row = $result->fetch_assoc()) {
+		$_SESSION[$row['name']] = $row['value'];
+	} // end while
+} // end if
+// ==============================================
 ?>
 
- 
+
 <style>
-.container{
-    position:center;
-    text-align:left;
-}
+	.container {
+		position: center;
+		text-align: left;
+	}
 </style>
 
 <div class="right-content">
-    <div class="container">
+	<div class="container">
 
-<style>#title {text-align: center;color: darkgoldenrod;}</style>
-<html>
-	<head>
-		<title>Preferences</title>
 		<style>
-			input {
-			text-align: center;
+			#title {
+				text-align: center;
+				color: darkgoldenrod;
 			}
 		</style>
-	</head>
-	<body>
-		<br>
+		<html>
+
+		<head>
+			<title>Preferences</title>
+			<style>
+				input {
+					text-align: center;
+				}
+			</style>
+		</head>
+
+		<body>
+			<br>
 			<h1 id="title">Update Preferences</h1><br>
-			</body>
-			<div class="container">
-                <?php
-                    if (isset($_GET['preferencesUpdated'])) {
-                        if ($_GET["preferencesUpdated"] == "Success") {
-                            echo "<br><h3 align=center style='color:green'>Success! The Preferences have been updated!</h3>";
-                        }
-                    }
-                ?>
-<!-- ======================================
-Here are the preferences we saved in $_SESSION in index dot php.
-DEFAULT_COLUMN_COUNT, 
-DEFAULT_LANGUAGE, 
-DEFAULT_HOME_PAGE_DISPLAY,  
-DEFAULT_CHUNK_SIZE, 
-NO_OF_QUOTES_TO_DISPLAY, 
-FEELING_LUCKY_MODE, 
-FEELING_LUCKY_TYPE
-Retrieve those and show the current values. 
-======================================-->
+		</body>
+		<div class="container">
+			<?php
+			if (isset($_GET['preferencesUpdated'])) {
+				if ($_GET["preferencesUpdated"] == "Success") {
+					echo "<br><h3 align=center style='color:green'>Success! The Preferences have been updated!</h3>";
+				}
+			}
+			?>
 
+			<form action="modifyThePreferences.php" method="POST" align=center>
+				<table style="width:500px" class="container">
+					<tr>
+						<th style="width:200px"></th>
+						<th>Current Value</th>
+						<th>Update Value</th>
+					</tr>
+					<tr>
+						<td class="width:200px">Default Column Count:</td>
+						<td><input disabled type="int" maxlength="2" size="15" value="<?php echo $_SESSION['DEFAULT_COLUMN_COUNT']; ?>" title="Current value"></td>
+						<td><input required type="int" name="column_count" maxlength="2" size="15" title="Enter the column count" value="<?php echo $_SESSION['DEFAULT_COLUMN_COUNT']; ?>"></td>
+					</tr>
+					<tr>
+						<td class="width:200px">Default Lanugage:</td>
+						<td><input disabled type="text" maxlength="15" size="15" value="<?php echo $_SESSION['DEFAULT_LANGUAGE']; ?>" title="Current value"></td>
+						<td><input required type="text" name="language" maxlength="10" size="15" title="Enter the language (English, Telugu)" value="<?php echo $_SESSION['DEFAULT_LANGUAGE']; ?>"></td>
+					</tr>
+					<tr>
+						<td class="width:200px">Default Home Page Display:</td>
+						<td><input disabled type="text" maxlength="4" size="15" value="<?php echo $_SESSION['DEFAULT_HOME_PAGE_DISPLAY']; ?>" title="Current value"></td>
+						<td><input required type="text" name="home_page_display" maxlength="10" size="15" title="Enter the display type for Home Page (Puzzles, Quote, Both)" value="<?php echo $_SESSION['DEFAULT_HOME_PAGE_DISPLAY']; ?>"></td>
+					</tr>
+					<tr>
+						<td style="width:200px">Default Chunk Size:</td>
+						<td><input disabled type="int" maxlength="1" size="15" value="<?php echo $_SESSION['DEFAULT_CHUNK_SIZE']; ?>" title="Current value"></td>
+						<td><input required type="int" name="chunk_size" maxlength="1" size="15" title="Enter the chunk size for splitter" value="<?php echo $_SESSION['DEFAULT_CHUNK_SIZE']; ?>"></td>
+					</tr>
+					<tr>
+						<td style="width:200px">No. of Quotes to display::</td>
+						<td><input disabled type="int" maxlength="3" size="15" value="<?php echo $_SESSION['NO_OF_QUOTES_TO_DISPLAY']; ?>" title="Current value"></td>
+						<td><input required type="int" name="no_of_quotes_to_display" maxlength="3" size="15" title="Enter the no. of quotes to display" value="<?php echo $_SESSION['NO_OF_QUOTES_TO_DISPLAY']; ?>"></td>
+					</tr>
+					<tr>
+						<td style="width:215px">Feeling Lucky Mode:</td>
+						<td><input disabled type="text" maxlength="10" size="15" value="<?php echo $_SESSION['FEELING_LUCKY_MODE']; ?>" title="Current value"></td>
+						<td><input required type="text" name="feeling_lucky_mode" maxlength="10" size="15" title="Enter the ID selection (FIRST, LAST or RANDOM)" value="<?php echo $_SESSION['FEELING_LUCKY_MODE']; ?>"></td>
+					</tr>
+					<tr>
+						<td style="width:200px">Feeling Lucky Type:</td>
+						<td><input disabled type="text" maxlength="10" size="15" value="<?php echo $_SESSION['FEELING_LUCKY_TYPE']; ?>" title="Current value"></td>
+						<td><input required type="text" name="feeling_lucky_type" maxlength="10" size="15" title="Enter the puzzle type (DropQuote, FloatQuote, DropFloat, Scrambler, Splitter, Slider16)" value="<?php echo $_SESSION['FEELING_LUCKY_TYPE']; ?>"></td>
+					</tr>
+				</table>
 
-				<form action="modifyThePreferences.php" method="POST" align=center>
-					<table style="width:500px" class="container">
-						<tr>
-							<th style="width:200px"></th>
-							<th>Current Value</th> 
-							<th>Update Value</th>
-						</tr>
-						<tr>
-							<td class="width:200px">Default Column Count:</td>
-							<td><input disabled type="int" maxlength="2" size="10" value="<?php echo $_SESSION['DEFAULT_COLUMN_COUNT']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="column_count" maxlength="2" size="10" title="Enter the column count" value="<?php echo $_SESSION['DEFAULT_COLUMN_COUNT']; ?>"></td>
-						</tr>
-						<tr>
-							<td class="width:200px">Default Lanugage:</td>
-							<td><input disabled type="int" maxlength="15" size="15" value="<?php echo $_SESSION['DEFAULT_LANGUAGE']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="language" maxlength="15" size="15" title="Enter the language" value="<?php echo $_SESSION['DEFAULT_LANGUAGE']; ?>"></td>
-						</tr>
-                        <tr>
-							<td class="width:200px">Default Home Page Display:</td>
-							<td><input disabled type="int" maxlength="4" size="10" value="<?php echo $_SESSION['DEFAULT_HOME_PAGE_DISPLAY']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="home_page_display" maxlength="4" size="10" title="Enter the home page display" value="<?php echo $_SESSION['DEFAULT_HOME_PAGE_DISPLAY']; ?>"></td>
-						</tr>
-                        <tr>
-							<td style="width:200px">Default Chunk Size:</td>
-							<td><input disabled type="int" maxlength="4" size="10" value="<?php echo $_SESSION['DEFAULT_CHUNK_SIZE']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="chunk_size" maxlength="4" size="10" title="Enter the chunk size for splitter" value="<?php echo $_SESSION['DEFAULT_CHUNK_SIZE']; ?>"></td>
-						</tr>
-                        <tr>
-							<td style="width:200px">No. of Quotes to display::</td>
-							<td><input disabled type="int" maxlength="2" size="10" value="<?php echo $_SESSION['NO_OF_QUOTES_TO_DISPLAY']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="no_of_quotes_to_display" maxlength="2" size="10" title="Enter the no. of quotes to display" value="<?php echo $_SESSION['NO_OF_QUOTES_TO_DISPLAY']; ?>"></td>
-						</tr>
-						<tr>
-							<td style="width:215px">Feeling Lucky Mode:</td>
-							<td><input disabled type="int" maxlength="2" size="10" value="<?php echo $_SESSION['FEELING_LUCKY_MODE']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="feeling_lucky_mode" maxlength="3" size="10" title="Enter FIRST, LAST or RANDOM" value="<?php echo $_SESSION['FEELING_LUCKY_MODE']; ?>"></td>
-						</tr>
-                        <tr>
-							<td style="width:200px">Feeling Lucky Type:</td>
-							<td><input disabled type="int" maxlength="4" size="10" value="<?php echo $_SESSION['FEELING_LUCKY_TYPE']; ?>" title="Current value"></td> 
-							<td><input required type="int" name="feeling_lucky_type" maxlength="4" size="10" title="Enter the puzzle type" value="<?php echo $_SESSION['FEELING_LUCKY_TYPE']; ?>"></td>
-						</tr>
-                    </table>
-                    
-                    <br>
-							<button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">UPDATE</button>
-				</form>
-			</div>
-	</body>
-</html>
+				<br>
+				<button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">UPDATE</button>
+			</form>
+		</div>
+		</body>
 
-    </div>
+		</html>
+
+	</div>
 </div>
 
 <?php include("footer.php"); ?>
